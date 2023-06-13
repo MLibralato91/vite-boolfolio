95f16f4215ff68837b28656314f221697a4f2023
MLibralato91 <michelelibralato911@gmail.com>
2023-06-13 16:36:18 +0200

    add pagination

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,27 @@
     <div class="container">
       <h1>{{ title }}</h1>
       <div class="row">
-        <div class="col" v-for="(project, index) in projects" :key="project.id">
-          {{ project.title }}
+        <div class="col-3" v-for="(project, index) in    projects   " :key="project.id">
+          <div class="card">
+            <img class="card-img-top" :src="project.image" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">{{ project.title }}</h5>
+              <p class="card-text">{{ project.description }}</p>
+              <a href="#" class="btn btn-primary">Dettagli</a>
+            </div>
+          </div>
         </div>
       </div>
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item"><button class="page-link" @click="getData(currentPage - 1)">Previous</button></li>
+          <li class="page-item" v-for="   n    in    lastPage   "><button class="page-link" @click="getData(n)">{{ n
+          }}</button>
+          </li>
+
+          <li class="page-item"><button class="page-link" @click="getData(currentPage + 1)">Next</button></li>
+        </ul>
+      </nav>
     </div>
   </div>
 </template>
@@ -14,21 +31,30 @@
 <script>
 
 import axios from 'axios';
-import { resolveTransitionHooks } from 'vue';
+
 
 export default {
   'name': 'App',
   data() {
     return {
-      title: 'Ciao',
+      title: 'My Projects',
       projects: [],
-      apiUrl: 'http://127.0.0.1:8000/api'
+      apiUrl: 'http://127.0.0.1:8000/api',
+      currentPage: 1,
+      lastPage: null,
     }
   },
   methods: {
-    getData() {
-      axios.get(`${this.apiUrl}/projects`).then((res) => {
+    getData(numPage) {
+      axios.get(`${this.apiUrl}/projects`, {
+        params: {
+          'page': numPage
+        }
+      }).then((res) => {
+        //console.log(res);
         this.projects = res.data.results.data;
+        this.currentPage = res.data.results.current_page;
+        this.lastPage = res.data.results.last_page;
       })
     }
   },
