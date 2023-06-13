3f3ea80ad770ae84f71acce487dfbf3c4646c527
MLibralato91 <michelelibralato911@gmail.com>
2023-06-13 14:25:37 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>{{ title }}</h1>
+      <div class="row">
+        <div class="col" v-for="(project, index) in projects" :key="project.id">
+          {{ project.name }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { axios } from 'axios';
+import { resolveTransitionHooks } from 'vue';
+
+export default {
+  'name': 'App',
+  data() {
+    return {
+      title: 'Ciao',
+      projects: [],
+      apiUrl: 'http://127.0.0.1:8000/api'
+    }
+  },
+  methods: {
+    getData() {
+      axios.get(`${this.apiUrl}/projects`).then((res) => {
+        this.projects = res.data.results.data;
+      })
+    }
+  },
+  mounted() {
+    this.getData();
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
